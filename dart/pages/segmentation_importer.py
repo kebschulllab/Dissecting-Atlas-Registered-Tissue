9689c3850e372925d6fd3f44172d2b3408072051
Rishi <rishikoneru2005@gmail.com>
2025-08-11 09:00:30 -0500

    feat: add functionality for displaying results

--- dart/pages/segmentation_importer.py
+++ dart/pages/segmentation_importer.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinter import ttk
 
 from pages.base import BasePage
+from utils import TkFigure
 
 class SegmentationImporter(BasePage):
     """
@@ -22,6 +23,9 @@ class SegmentationImporter(BasePage):
         """
         Create the widgets for this page. This includes:
         - Instructions label: A label with instructions for the user to follow
+        - Load Button: A button that will call self.load when clicked
+        - - Results Frame: Contains figure displaying results along with
+        navigation controls
         """
         self.instructions_label = ttk.Label(
             master=self,
@@ -33,6 +37,9 @@ class SegmentationImporter(BasePage):
             text="Load Segmentation(s)",
             command=self.load
         )
+
+        self.results_viewer = tk.Frame()
+        self.create_result_viewer()
     
     def show_widgets(self):
         """
@@ -62,6 +69,158 @@ class SegmentationImporter(BasePage):
         """
         # TODO: implement me
         return
+
+    def create_result_viewer(self):
+        """
+        Create the widgets in the results frame. These include:
+        - Menu Frame: Contains navigation controls for viewing results
+        of each target.
+        - Slice Frame: Contains the figure to display the target image
+        with the boundaries from the segmentation overlaid.
+        """
+        
+        # for showing results after running stalign
+        self.menu_frame = tk.Frame(self.results_viewer)
+        self.slice_frame = tk.Frame(self.results_viewer)
+
+        self.slide_nav_label = ttk.Label(self.menu_frame, text="Slide: ")
+        self.curr_slide_var = tk.IntVar(master=self.menu_frame, value='1')
+        self.slide_nav_combo = ttk.Combobox(
+            master=self.menu_frame,
+            values=[],
+            state='readonly',
+            textvariable=self.curr_slide_var,
+        )
+        self.slide_nav_combo.bind('<<ComboboxSelected>>', self.switch_slides)
+
+        self.target_nav_label = ttk.Label(self.menu_frame, text="Target: ")
+        self.curr_target_var = tk.IntVar(master=self.menu_frame, value='1')
+        self.target_nav_combo = ttk.Combobox(
+            master=self.menu_frame,
+            values=[],
+            state='readonly',
+            textvariable=self.curr_target_var,
+        )
+        self.target_nav_combo.bind('<<ComboboxSelected>>', self.update_result_viewer)
+
+        self.slice_viewer = TkFigure(self.slice_frame, toolbar=True)
+
+    def show_result_viewer(self):
+        """
+        Show the results viewer. This method displays the widgets 
+        inside the results frame with pack and grid configuration.
+        """
+        
+        self.results_viewer.grid_rowconfigure(1, weight=1)
+        self.results_viewer.grid_columnconfigure(0, weight=1)
+        self.menu_frame.grid(row=0, column=0, sticky='nsew')
+        self.slice_frame.grid(row=1, column=0, sticky='nsew')
+
+        self.slide_nav_label.pack(side=tk.LEFT)
+        self.slide_nav_combo.pack(side=tk.LEFT)
+        
+        self.target_nav_combo.pack(side=tk.RIGHT)
+        self.target_nav_label.pack(side=tk.RIGHT)
+
+        self.slice_viewer.get_widget().pack(expand=True, fill=tk.BOTH)
+    
+    def update_result_viewer(self, event=None):
+        """
+        Update the figure displaying the result. This method gets the
+        current slide and target indices, ensures the dropdowns are 
+        correctly configured, and displays the results for the current
+        target.
+
+        Parameters
+        ----------
+        event : tk.Event, optional
+            The event that triggered the update (default is None).
+        """
+        
+        self.currSlide = self.slides[self.get_slide_index()]
+        self.currTarget = self.currSlide.targets[self.get_target_index()]
+        self.target_nav_combo.config(
+            values=[i+1 for i in range(self.currSlide.numTargets)]
+        )
+        self.show_seg()
+
+    def show_seg(self):
+        """
+        Show the current target with the region boundaries overlaid.
+        This method clears the results display then shows the results
+        for the current target.
+        """
+        
+        self.slice_viewer.axes[0].cla()
+
+        if 'stalign' in self.currTarget.seg:
+            seg_img = self.currTarget.get_img(seg='stalign')
+        else:            
+            seg_img = self.currTarget.get_img()
+        
+        self.slice_viewer.axes[0].imshow(seg_img)
+        self.slice_viewer.update()
+
+    def show_results(self):
+        """
+        Show the results frame. This method updates the figure to display
+        the current target's results, ensures the navigation controls are 
+        correctly configured, and packs the results frame so that it's 
+        visible.
+        """
+
+        self.results_viewer.pack(expand=True, fill=tk.BOTH)
+        self.show_result_viewer()
+
+        self.currSlide = None
+        self.currTarget = None
+        self.update_result_viewer()
+        self.slide_nav_combo.config(
+            values=[i+1 for i in range(len(self.slides))]
+        )
+
+    def switch_slides(self, event=None):
+        """
+        Switch to the selected slide in the slide navigation combobox. This method
+        retrieves the selected slide index from the combobox, updates the current
+        slide, and updates the target navigation combobox to reflect the targets
+        available for the selected slide. It also resets the new points and updates
+        the target and atlas images in the slice viewer.
+        
+        Parameters
+        ----------
+        event : tk.Event, optional
+            The event that triggered the switch (default is None).
+        """
+        self.curr_target_var.set(1)
+        self.update_result_viewer()
+    
+    def get_slide_index(self):
+        """
+        Get the index of the current slide based on the selected value in the
+        slide navigation combobox. The index is adjusted to be zero-based by
+        subtracting 1 from the selected value.
+        
+        Returns
+        -------
+        index : int
+            The index of the current slide.
+        """
+        return self.curr_slide_var.get()-1
+    
+    def get_target_index(self):
+        """
+        Get the index of the current target based on the selected value in the
+        target navigation combobox. The index is adjusted to be zero-based by 
+        subtracting 1 from the selected value.
+        
+        Returns
+        -------
+        index : int
+            The index of the current target.
+        """
+
+        return self.curr_target_var.get()-1
 
     def activate(self):
         """
