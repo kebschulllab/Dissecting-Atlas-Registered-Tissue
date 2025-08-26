c166f8cb6ffe1c67bcda4e139b33411f94b5ae36
Rishi <rishikoneru2005@gmail.com>
2025-08-26 10:11:47 -0400

    feat: complete load_slide_processor

--- dart/test/load.py
+++ dart/test/load.py
@@ -1,4 +1,10 @@
+import json
+import tkinter as tk
+import os
+
 from ..app import Project
+from ..images import Slide
+from ..pages import *
 
 def load_project(page_name):
     """
@@ -46,8 +52,57 @@ def load_starter():
     return Project()
 
 def load_slide_processor():
+    """
+    Load the project state for the SlideProcessor page. This function builds
+    upon the Starter page's project state. It loads the slides and atlases and
+    creates a temporary project folder.
+    
+    Returns:
+    --------
+    Project
+        The project object to be used in the SlideProcessor page.
+    """
     project = load_starter()
-    # TODO: implement
+    dummy_master = tk.Tk()
+    dummy_starter = Starter(dummy_master, project)
+
+    # load atlas
+    print("Loading atlas", end='...')
+    atlas_json_path = os.path.join(
+        os.path.dirname(__file__), 
+        'data',
+        'atlas.json'
+    )
+    with open(atlas_json_path, 'r') as f:
+        atlas_name = json.load(f)
+    dummy_starter.load_atlas_info(atlas_name)
+    print("COMPLETE")
+
+    # load slides
+    print("Loading slides", end='...')
+    dummy_starter.load_slides(os.path.join(
+        os.path.dirname(__file__), 
+        '..', 
+        '..',
+        'demo_images'
+    ))
+    print("COMPLETE")
+
+    # create temporary project folder
+    print("Creating temporary project folder", end='...')
+    project.parent_folder = os.path.abspath(os.path.dirname(__file__))
+    project.folder = os.path.join(
+        project.parent_folder,
+        "DART-temp"
+    )
+    os.mkdir(project.folder)
+    print("COMPLETE")
+
+    # clean up dummy master (will destroy dummy_starter as well)
+    dummy_master.update()
+    dummy_master.update_idletasks()
+    dummy_master.destroy()
+
     return project
 
 def load_target_processor():
