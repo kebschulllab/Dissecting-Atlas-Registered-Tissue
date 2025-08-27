ac6f97eb1904d45f09688b1b7d737ec1a6adc250
Rishi <rishikoneru2005@gmail.com>
2025-08-27 00:13:47 -0400

    test: load completed_slide_processor and test SlideProcessor.done

--- dart/test/test_slide_processor.py
+++ dart/test/test_slide_processor.py
@@ -1,3 +1,4 @@
+import imageio as iio
 import numpy.testing as npt
 import pytest
 import shutil
@@ -6,6 +7,7 @@ import os
 
 from ..pages import SlideProcessor
 from .load import load_slide_processor
+from ..utils import get_target_name
 
 
 @pytest.fixture(scope="module")
@@ -13,7 +15,6 @@ def project():
 	loaded_project = load_slide_processor()
 	yield loaded_project
 	shutil.rmtree(loaded_project.folder)
-    
 
 @pytest.fixture(scope="module")
 def master():
@@ -29,6 +30,57 @@ def slide_processor(master, project):
 def activated_slide_processor(slide_processor):
 	slide_processor.activate()
 	return slide_processor
+
+def get_target_data(line):
+    target_name, coords = line.strip().split(' : ')
+    img = iio.imread(os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "data",
+        f"{target_name}.png"
+    ))
+
+    sn, tn = [int(s[-1]) for s in target_name.split('_')]
+    x, y = map(int, coords.split())
+    h, w = img.shape[:2]
+    return sn, tn, x, y, h, w
+
+def get_calibration_point_data(line):
+    sn, coords = line.strip().split(' : ')
+    sn = int(sn)
+    x, y = map(int, coords.split())
+    return sn, x, y
+
+@pytest.fixture
+def completed_slide_processor(activated_slide_processor):
+    sp = activated_slide_processor
+
+    # load targets
+    target_coords_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "data",
+        "target_coordinates.txt"
+    )
+    with open(target_coords_path, 'r') as f:
+        f.readline()
+        for line in f.readlines():
+            sn, _, x, y, h, w = get_target_data(line)
+            data = sp.project.slides[sn-1].get_img()[y:y+h, x:x+w]
+
+            sp.project.slides[sn-1].add_target(x, y, data)
+
+    # load calibration points
+    calib_coords_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "data",
+        "calibration_points.txt"
+    )
+    with open(calib_coords_path, 'r') as f:
+        f.readline()
+        for line in f.readlines():
+            sn, x, y = get_calibration_point_data(line)
+            sp.project.slides[sn-1].add_calibration_point([x, y])
+
+    return sp
 
 def test_init(slide_processor):
 	assert hasattr(slide_processor, "header")
@@ -58,28 +110,6 @@ def test_refresh(activated_slide_processor):
     sp.curr_slide_var.set(numSlides)
     with pytest.raises(IndexError):
         sp.refresh()
-'''
-@pytest.mark.parametrize("annotation_mode", ["point", "rect"])
-def test_update_buttons(activated_slide_processor):
-    sp = activated_slide_processor
-    numSlides = len(sp.slides)
-
-    sp.curr_slide_var.set(1)
-    sp.update_buttons()
-    assert sp.prev_button['state'] == 'disabled'
-    assert sp.next_button['state'] == 'normal'
-
-    sp.curr_slide_var.set(numSlides)
-    sp.update_buttons()
-    assert sp.prev_button['state'] == 'normal'
-    assert sp.next_button['state'] == 'disabled'
-
-    if numSlides > 2:
-        sp.curr_slide_var.set(2)
-        sp.update_buttons()
-        assert sp.prev_button['state'] == 'normal'
-        assert sp.next_button['state'] == 'normal'
-'''
 
 @pytest.mark.parametrize("equal_click_release", [True, False])
 def test_on_select(activated_slide_processor, equal_click_release):
@@ -141,19 +171,36 @@ def test_get_index(activated_slide_processor):
 	sp.curr_slide_var.set(numSlides)
 	assert sp.get_index() == numSlides - 1
 
-def test_clear(completed_slide_processor):
+def test_done(completed_slide_processor):
     sp = completed_slide_processor
-    sp.clear()
-	
-    assert sp.newPointX == -1
-    assert sp.newPointY == -1
-    assert sp.newTargetX == -1
-    assert sp.newTargetY == -1
-    assert sp.newTargetData is None
+    sp.done()
+    
+    # Check that calibration points are saved correctly
+    cp_results_path = os.path.join(sp.project.folder, "calibration_points.txt")
+    assert os.path.exists(cp_results_path)
+    cp_expected_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "data",
+        "calibration_points.txt"
+    )
+    with open(cp_results_path, 'r') as f_act, open(cp_expected_path, 'r') as f_exp:
+        lines_act = f_act.readlines()
+        lines_exp = f_exp.readlines()
+        for la, le in zip(lines_act, lines_exp):
+            assert la.strip() == le.strip()                   
 
-'''
-def test_cancel(master, project):
-	sp = SlideProcessor(master, project)
-	# Should call super().cancel() without error
-	sp.cancel()'''
+    # Check that target images are saved correctly
+    for si, slide in enumerate(sp.slides):
+        for ti, target in enumerate(slide.targets):
+            act_target_path = os.path.join(sp.project.folder, get_target_name(si, ti) + ".png")
+            exp_target_path = os.path.join(
+                os.path.dirname(os.path.abspath(__file__)),
+                "data",
+                get_target_name(si, ti) + ".png"
+            )
+            
+            assert os.path.exists(act_target_path)
+            saved_img_act = iio.imread(act_target_path)
+            saved_img_exp = iio.imread(exp_target_path)
+            npt.assert_array_equal(saved_img_act, saved_img_exp)
 
