2b326324b80fbc3ff389bae5cf5130e429807f6d
Rishi <rishikoneru2005@gmail.com>
2025-08-31 10:16:53 -0400

    remove unneeded helper funcs

--- dart/test/test_stalign_runner.py
+++ dart/test/test_stalign_runner.py
@@ -29,67 +29,6 @@ def completed_stalign_runner(activated_stalign_runner):
     sr.run()
     return sr
 
-def load_targets(sp, data):
-    """
-    Load targets using the provided data and the SlideProcessor interface.
-    Activates rectangle annotation mode. Then, for each target, sets the 
-    current slide to that slide and simulates a rectangle selection event on
-    the target coordinates followed by a commit.
-    
-    Parameters
-    ----------
-    sp : SlideProcessor
-        The SlideProcessor instance where targets will be added.
-    data : list of dict
-        A list of dictionaries containing slide numbers, target numbers, and
-        their corresponding coordinates and shapes.
-    """
-
-    sp.activate_rect_mode()
-    for target in data:
-        sn = target['slide_index']
-        x = target['x_offset']
-        y = target['y_offset']
-        t_shape = target['shape'][:2]
-
-        sp.curr_slide_var.set(sn + 1)
-        sp.refresh()
-
-        click = DummyEvent(x, y)
-        release = DummyEvent(x + t_shape[1], y + t_shape[0])
-        sp.on_select(click, release)
-        sp.commit()
-
-def load_calibration_points(sp, data):
-    """
-    Load calibration points using the provided data and the SlideProcessor 
-    interface. Activates point annotation mode, then sets the SlideProcessor 
-    to point mode. For each slide, sets the current slide to that slide and for
-    each calibration point, simulates a click event on the point and a click on
-    the commit button.
-    
-    Parameters
-    ----------
-    sp : SlideProcessor
-        The SlideProcessor instance where calibration points will be added.
-    data : list of dict
-        A list of dictionaries containing slide numbers and their corresponding
-        calibration points.
-    """
-
-    sp.activate_point_mode()
-    for slide in data:
-        sn = slide['slide_index']
-        sp.curr_slide_var.set(sn + 1)
-        sp.refresh()
-        for point in slide['points']:
-            x = point[0]
-            y = point[1]
-            axes = sp.slide_viewer.axes[0]
-            event = DummyEvent(x, y, inaxes=axes, button=1)
-            sp.on_click(event)
-            sp.commit()
-
 def test_done(completed_stalign_runner):
     sr = completed_stalign_runner
     sr.done()
