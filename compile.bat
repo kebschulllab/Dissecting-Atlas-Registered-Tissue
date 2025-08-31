da53f6d89f05a804f56ccbda3f36a1f33f496adc
mmanant <manjari.anant@gmail.com>
2025-08-31 16:49:36 -0400

    update compile instructions to adjust for new dart files tructure

--- compile.bat
+++ compile.bat
@@ -1,5 +1,1 @@
 pyinstaller main.spec --noconfirm
-move ".\dist\dart\_internal\pages" "dist\dart\pages"
-move ".\dist\dart\_internal\atlases" "dist\dart\atlases"
-move ".\dist\dart\_internal\resources" "dist\dart\resources"
-move ".\dist\dart\_internal\VisuAlign-v0_9" "dist\dart\VisuAlign-v0_9"
