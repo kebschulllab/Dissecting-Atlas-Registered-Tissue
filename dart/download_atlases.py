85e0625480c777d9d6b2c2f7eba6435369b6d80a
mmanant <manjari.anant@gmail.com>
2026-01-28 10:10:44 -0500

    complete functionality of atlas downloading feat

--- dart/download_atlases.py
+++ dart/download_atlases.py
@@ -6,7 +6,8 @@ import requests
 
 
 RECORD_ID = 17849564  # Zenodo record id containing the atlases archive
-ZENODO_URL = f"https://zenodo.org/record/{RECORD_ID}/files/atlases.zip"
+BACKUP_DOWNLOAD_URL = f"https://zenodo.org/record/{RECORD_ID}/files/atlases.zip"
+LAST_KNOWN_SIZE = 2851427875  # last known size in bytes of atlas.zip
 
 # Default destination: package-local `atlases` directory next to this module
 DEST_DIR = os.path.join(
@@ -29,17 +30,46 @@ def download_atlases():
     dest_dir = Path(DEST_DIR)
     dest_dir.mkdir(parents=True, exist_ok=True)
 
+    # Get record
+    print("Fetching atlas metadata from Zenodo...",end="")
+    try:
+        record = requests.get(f"https://zenodo.org/api/records/{RECORD_ID}")
+        record.raise_for_status()
+        record = record.json()
+        download_url = record['files'][0]['links']['self']
+        size = record['files'][0]['size']
+        print("DONE")
+    except:
+        print("FAILED")
+        print("Failed to fetch metadata from Zenodo. Using backup download URL.")
+        download_url = BACKUP_DOWNLOAD_URL
+        size = LAST_KNOWN_SIZE
+
     # Download .zip file from zenodo
-    atlases_zip = requests.get(ZENODO_URL, stream=False)
     dest_file = os.path.join(dest_dir, "atlases.zip")
-    with open(dest_file, 'wb') as f:
-        f.write(atlases_zip.content)
-    
+    with requests.get(download_url, stream=True) as r:
+        r.raise_for_status()
+        with open(dest_file, 'wb') as f:
+            print(f"Downloading atlases.zip...(0/{size})",end='\r')
+            downloaded = 0
+            for chunk in r.iter_content(8192):
+                if chunk:
+                    f.write(chunk)
+                    downloaded += len(chunk)
+                    print(f"Downloading atlases.zip...({downloaded}/{size})", end='\r')
+            print("Downloading atlases.zip.......................................DONE")
+        
     # Extract .zip file
     with ZipFile(dest_file, 'r') as zip_ref:
+        print("Extracting atlases.zip", end='...')
         zip_ref.extractall(dest_dir)
+        print("DONE")
 
-        
+    # Remove .zip file
+    print("Removing atlases.zip", end='...')
+    os.remove(dest_file)
+    print("DONE")
+
     return dest_dir
 
 def main():
